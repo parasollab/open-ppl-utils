f7dd36c053891722f50cd06882c5ea2a9d6c3cb2
Read Sandstrom <readamus@tamu.edu>
2018-09-01 16:06:46 +0000

    Adding new bullet version in attempt to fix suspected memory corruption issue.
    git-svn-id: svn+ssh://parasol-svn.cs.tamu.edu/research/parasol-svn/svnrepository/utils@224 720011fe-810a-0410-ac5b-998ee3860a72

---
+++ bullet/2.87/parasol_setup.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/sh
+#
+# Setup the bullet library for use with PMPL.
+
+# Configure directory layout.
+base_dir=$(pwd)
+build_dir=${base_dir}/build
+install_dir=${build_dir}/install
+
+# Remove existing build and install directories if they exist.
+rm -rf ${build_dir} ${install_dir}
+
+# Create the build and install directories, and generate the makefile.
+mkdir ${build_dir} ${install_dir}
+cd ${build_dir}
+cmake -G"Unix Makefiles" \
+  -DBUILD_BULLET2_DEMOS=OFF \
+  -DBUILD_BULLET3=ON \
+  -DBUILD_CLSOCKET=OFF \
+  -DBUILD_CPU_DEMOS=OFF \
+  -DBUILD_ENET=OFF \
+  -DBUILD_EXTRAS=ON \
+  -DBUILD_OPENGL3_DEMOS=OFF \
+  -DBUILD_PYBULLET=OFF \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DBUILD_UNIT_TESTS=OFF \
+  -DBULLET2_USE_THREAD_LOCKS=OFF \
+  -DCLSOCKET_DEP_ONLY=OFF \
+  -DCLSOCKET_SHARED=OFF \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_CXX_FLAGS_RELWITHDEBINFO="-O2 -g3 -DNDEBUG" \
+  -DCMAKE_C_FLAGS_RELWITHDEBINFO="-O2 -g3 -DNDEBUG" \
+  -DCMAKE_STATIC_LINKER_FLAGS_RELWITHDEBINFO="" \
+  -DCMAKE_INSTALL_PREFIX=${install_dir} \
+  -DINSTALL_CMAKE_FILES=OFF \
+  -DINSTALL_EXTRA_LIBS=ON \
+  -DINSTALL_LIBS=ON \
+  -DUSE_CUSTOM_VECTOR_MATH=OFF \
+  -DUSE_DOUBLE_PRECISION=ON \
+  -DUSE_GLUT=OFF \
+  -DUSE_GRAPHICAL_BENCHMARK=OFF \
+  ../
+
+
+# Bullet takes a while to build, so use parallel make with max number of
+# processors.
+numprocs=$(grep -c ^processor /proc/cpuinfo)
+make install -j${numprocs}
